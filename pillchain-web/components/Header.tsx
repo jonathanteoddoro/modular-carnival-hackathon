fc695ae48f6fde10f8dfdfd8db7408ef7bf1bc3f
Davi Abreu da Silveira <davi.silveira@sou.inteli.edu.br>
2025-02-27 17:23:13 -0300

    header changes

--- pillchain-web/components/Header.tsx
+++ pillchain-web/components/Header.tsx
@@ -3,14 +3,11 @@
 import Image from "next/image";
 
 function Header() {
-
   return (
-    <>
-    <header className="h-20 w-full p-4 sm:px-[76px] flex justify-center bg-red-500 shadow-md">
-        <Image src="/logo.svg" alt="Pillchain" width={100} height={100} />
+    <header className="h-20 w-full p-4 sm:px-[76px] flex justify-between items-center bg-red-500 shadow-md">
+      <h1 className="text-white text-2xl font-bold">Pill Chain</h1>
+      <Image src="/logo.svg" alt="Pill chain" width={50} height={100} />
     </header>
-    
-    </>
   );
 }
 
